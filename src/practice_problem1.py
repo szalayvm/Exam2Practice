7ff738dc06d8e27934f08eca6cad772f17e80de0
szalayvm <szalayvm>
2018-01-03 15:57:27 -0500

    Finished first TODO, work on later

--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  IMPLEMENTING CLASSES.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Victoria Szalay.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -39,8 +39,8 @@ def main():
     # UN-comment tests as you work the problems.
     ####################################################################
 
-#     run_test_init()
-#     run_test_append_string()
+    run_test_init()
+    run_test_append_string()
 #     run_test_double()
 #     run_test_shrink()
 #     run_test_double_then_shrink()
@@ -94,7 +94,7 @@ class Box(object):
           :type volume: int
         """
         # --------------------------------------------------------------
-        # TODO: 2. Implement and test this function.
+        # Done: 2. Implement and test this function.
         #     See the testing code (below) for more examples.
         # --------------------------------------------------------------
         # --------------------------------------------------------------
@@ -102,6 +102,13 @@ class Box(object):
         #    DIFFICULTY:      3
         #    TIME ESTIMATE:   5 minutes.
         # --------------------------------------------------------------
+        self.contents = contents
+        self.volume = volume
+        if len(self.contents)>self.volume:
+            self.contents = ''
+
+        self.original = self.contents
+
 
     def append_string(self, additional_contents):
         """
@@ -156,6 +163,19 @@ class Box(object):
         #       Read_this_ONLY_when_asked_Part_2.txt
         #    and continue working on the problem.
         # --------------------------------------------------------------
+
+        if len(self.contents) + len(additional_contents) > self.volume:
+            newstring = ''
+            for g in range(0,self.volume-len(self.contents)):
+                self.contents = self.contents + str(additional_contents[g])
+            for k in range(self.volume-len(self.contents), len(additional_contents)-1):
+                newstring = newstring + additional_contents[k]
+            return newstring
+        else:
+            self.contents = self.contents + additional_contents
+            return ''
+
+
 
     def double(self):
         """
